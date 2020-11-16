87ca77238ace0a7aad1d3cce56a172b13a58c47b
adeyemijohn <adeyemijohnayodele@gmail.com>
2020-11-16 14:09:03 +0100

    New commit
    Joblister Web Application

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include_once 'config/init.php'; ?>
+
+<?php 
+$job = new Job;
+
+$template = new Template('templates.frontpage.php');
+
+$category = isset($_GET['category']) ? $_GET['category'] : null;
+
+if($category){
+	$template->job = $job->getByCategory($category);
+	$template->title = 'Jobs In '. $job->getCategory($category)->name;
+}else{
+	$template->title = 'Latest Jobs';
+	$template->job  = $job->getAllJob();
+
+}
+$template->categories = $job->getCategories();
+
+echo $template;
